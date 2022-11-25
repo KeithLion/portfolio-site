cd43b523d9c548ef99a0d02d9b399f98a0179413
KeithLion <nortonkiara6@gmail.com>
2022-11-24 20:18:53 -0800

    cleaned up the styling, created a temporary home button, and changed the paper to main

--- src/Views/AboutMe.js
+++ src/Views/AboutMe.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
 import Link from '@mui/material/Link'
+import { color } from '@mui/system'
 
 
 
 const AboutMe =() =>{
 
 
+    var boxStyle= {
+        box:{
+
+display:'flex', 
+flexDirection:'column',
+margin:'auto', 
+justifyContent:'center',
+alignItems:'center',
+minHeight:'100vh',
+backgroundColor:'#042940',
+color:'white'
+        }
+    }
+
 
     return(
-        <Box sx={{maxWidth: 800,display:'flex', flexDirection:'column',margin:'auto', minHeight:'100vh'}}>
-            <Paper sx={{padding:1, marginTop:20}}>
+        <Box sx={boxStyle.box}>
+            <main style={{maxWidth:800}}>
 
             <h1>About Me</h1>
             <p>
             I'm a front-end developer that started my journey learning though a workshop. in an effort to learn more joined a full stack bootcamp. I ended up circling back around to just front end design. I have had an eye for design since highschool and have found ways of designing projects from there. 
             </p>
-            </Paper>
+            </main>
             <footer>
             <Link href='#'>social links</Link>
+            <Link href='/' style={{display:'block'}}>Home</Link>
             </footer>
         </Box>
     )
